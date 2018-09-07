558de29877d95b8d3529bbcabe9bd2c159398abb
aittis-rctr <>
2018-09-07 17:00:28 +0300

    Simple title change test for CI pipeline

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Codemoth Coding Company',
   },
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
 }
