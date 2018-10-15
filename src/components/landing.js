3e23e0be6f22d59967d644163c4d4d2ef44ce37f
aittis-rctr <>
2018-10-15 14:42:50 +0300

    changed frontpage

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,23 +1,17 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import bg from '../images/background-4.jpg'
-
-console.log(bg)
+import bg from '../images/bg2.svg'
 
 const Landing = () => (
-  <section className="hero is-fullheight is-dark">
+  <section className="hero is-fullheight" style={{backgroundImage: `url(${bg})`}}>
     <div className="hero-body is-centered">
-      <div className="container is-fluid has-text-centered">
-      <p className="title is-size-1">Codemoth</p>
-      <p className="subtitle"><strong className="has-text-danger">coding company</strong></p>
+      <div className="has-text-centered" style={{margin: "0 auto"}}>
+        <div className="big-title-box" style={{backgroundColor: '#FFF'}}>
+          <p className="big-title">CODEMOTH</p>
+        </div>
       </div>
     </div>
 
-  <div className="hero-foot">
-      <div className="container has-text-centered">
-        <i className="far fa-arrow-square-down fa-3x" style={{paddingBottom: '10px'}}></i>
-      </div>
-  </div>
   </section>
 )
 
